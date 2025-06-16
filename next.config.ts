167e7ed54f148e19c34a4934009b6cdc01e6b469
Rehan Khna <rehan.khan@republic.com>
2025-06-16 13:27:56 +0530

    image base path added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+
+const isProduction = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: isProduction ? "/photown-web" : "",
   output: "export",
   distDir: "dist",
   trailingSlash: true,
