da2823eab006ca3e0ee471234eff08eb46835ae5
Rehan Khna <rehan.khan@republic.com>
2025-06-16 20:05:11 +0530

    repo path added

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
   output: 'export', // Important for static HTML export
   trailingSlash: true, // Ensures routes become `/about/` instead of `/about`
-  assetPrefix: isProd ? '/your-repo-name/' : '',
-  basePath: isProd ? '/your-repo-name' : '',
+  assetPrefix: isProd ? '/photown-web/' : '',
+  basePath: isProd ? '/photown-web' : '',
   distDir: 'dist', // Output directory for static files
 };
