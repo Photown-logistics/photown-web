795029417d754cdda30a22a2f6d1f4c334491837
Rehan Khna <rehan.khan@republic.com>
2025-06-16 19:32:42 +0530

    Try1

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export', // Important for static HTML export
-  distDir: 'build', // Custom output directory
+  distDir: 'output', // Custom output directory
   trailingSlash: true, // Ensures routes become `/about/` instead of `/about`
   assetPrefix: isProd ? '/photown-web/' : '',
   basePath: isProd ? '/photown-web' : '',
