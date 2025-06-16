996882323c8e9b4b519ae23579b1cc866095dff7
Rehan Khna <rehan.khan@republic.com>
2025-06-17 00:25:35 +0530

    path remove

--- next.config.js
+++ next.config.js
@@ -3,7 +3,5 @@ const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
   output: 'export', // Important for static HTML export
   trailingSlash: true, // Ensures routes become `/about/` instead of `/about`
-  assetPrefix: isProd ? '/photown-web/' : '',
-  basePath: isProd ? '/photown-web' : '',
   distDir: 'dist', // Output directory for static files
 };
