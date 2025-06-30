8e0850feec9281ba6279ee6874c0e34183993830
Rehan Khna <rehan.khan@republic.com>
2025-06-30 10:53:50 +0530

    GA added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "../components/Footer";
 import Header from "@/components/Header";
+import { GoogleAnalytics } from '@next/third-parties/google'
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -42,6 +44,7 @@ export default function RootLayout({
         </main>
         <Footer />
       </body>
+      <GoogleAnalytics gaId="G-TWGBJ25FML" />
     </html>
   );
 }
